f360ef21942b0b15a85e58754c5133a7b8b20fcc
walenzack <luisgonzalosotoaboal@gmail.com>
2019-01-03 10:27:53 +0100

    Update HideCommentsSE.user.js

--- HideCommentsSE.user.js
+++ HideCommentsSE.user.js
@@ -17,54 +17,57 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
-    var CLEAR_INBOX_INTERVAL_MILLIS = 5 * 1000;
-    var INBOX_BUTTON = 'a[class*="js-inbox-button"]';
-    var INBOX_ITEM = 'li[class^="inbox-item"]';
-    var COMMENT = 'div[id^="comments"]';
-    var SO = /stackoverflow\.com/;
-    var SE = /(\.stackexchange\.com|superuser\.com|askubuntu\.com|serverfault\.com|stackapps\.com|mathoverflow\.net)/;
-    var CHAT = /chat\.stackexchange\.com/;
+  'use strict';
+  var CLEAR_INBOX_INTERVAL_MILLIS = 5 * 1000;
+  var INBOX_BUTTON = 'a[class*="js-inbox-button"]';
+  var INBOX_ITEM = 'li[class^="inbox-item"]';
+  var COMMENT = 'div[id^="comments"]';
+  var SO = /stackoverflow\.com/;
+  var SE = /(\.stackexchange\.com|superuser\.com|askubuntu\.com|serverfault\.com|stackapps\.com|mathoverflow\.net)/;
+  var CHAT = /chat\.stackexchange\.com/;
 
-    var inboxUnreadCounter = document.querySelectorAll(INBOX_BUTTON)[0].children[1];
+  var inboxUnreadCounter = document.querySelectorAll(INBOX_BUTTON)[0].children[1];
 
-    function hideComments() {
-        [].forEach.call(
-            document.querySelectorAll(COMMENT),
-            function (el) {
-                el.style.display="none";
+  function hideComments() {
+    [].forEach.call(
+      document.querySelectorAll(COMMENT),
+      function (el) {
+        el.style.display="none";
+      }
+    );
+  }
+
+  function hideInboxComments() {
+    // TODO Skip forEach if there are no new notifications since last call
+    [].forEach.call(
+      document.querySelectorAll(INBOX_ITEM),
+      function (el) {
+        var href = el.children[0].href;
+        if (SE.test(href)) {
+          if (!CHAT.test(href)) { // Do not hide chat notifications
+            var type = el.children[0].children[1].children[0].children[0].innerText; //hacky af
+            if ('comment' === type) {
+              el.style.display="none";
+              var count = Number(inboxUnreadCounter.innerText);
+              if (count > 1) {
+                inboxUnreadCounter.innerText = --count;
+              } else if (count === 1) {
+                inboxUnreadCounter.innerText = "";
+                inboxUnreadCounter.style.display = 'none';
+              }
             }
-        );
-    }
+          }
+        }
+      }
+    );
+  };
 
-    function hideInboxComments() {
-        [].forEach.call(
-            document.querySelectorAll(INBOX_ITEM),
-            function (el) {
-                var href = el.children[0].href;
-                if (SE.test(href)) {
-                    if (!CHAT.test(href)) { // Do not hide chat notifications
-                        var type = el.children[0].children[1].children[0].children[0].innerText; //hacky af
-                        if ('comment' === type) {
-                            el.style.display="none";
-                            var count = Number(inboxUnreadCounter.innerText);
-                            if (count > 1) {
-                                inboxUnreadCounter.innerText = --count;
-                            } else if (count === 1) {
-                                inboxUnreadCounter.innerText = "";
-                                inboxUnreadCounter.style.display = 'none';
-                            }
-                        }
-                    }
-                }
-            }
-        );
-    };
-
-    // Main logic
-    if (!SO.test(window.location)) { // Do not hide SO comments
-        hideComments();
-    }
-    var timerClearInbox = window.setInterval(hideInboxComments, CLEAR_INBOX_INTERVAL_MILLIS);
-
+  // Main logic
+  if (!SO.test(window.location)) { // Do not hide SO comments
+    hideComments();
+  }
+  var timerClearInbox = window.setInterval(
+    hideInboxComments,
+    CLEAR_INBOX_INTERVAL_MILLIS
+  );
 })();
